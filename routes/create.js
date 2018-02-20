cc52ec2e7a36726c1ddb57edf15a141d2a676bec
Funarp <mr.function.sharp@gmail.com>
2018-02-21 00:08:27 +0800

    add preview page and pdf output

--- routes/create.js
+++ routes/create.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+//create application/x-www-form-urlencoded decoder
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/', function(req, res, next) {
     res.render('create', { title: 'Create - EasyMark' });
+});
+
+router.post('/preview', urlencodedParser, function(req, res, next) {
+    var md = req.body.md;
+    var act = req.body.act;
+    if (act === "pdf") {
+        res.render('preview', { title: 'Preview - EasyMark', md: md, action: "window.print()" });
+    } else {
+        res.render('preview', { title: 'Preview - EasyMark', md: md, action: "" });
+    }
 });
 
 module.exports = router;
